2f403df76fd11d91ee142a912a0ebe87d102a52d
gary <feiyang@live.cn>
2019-11-27 17:29:43 +0800

    0.1 数据库方法和主逻辑更新

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify, render_template, request, send_from_directory, redirect
+from flask import Flask, jsonify, render_template, request, send_from_directory, redirect, url_for, flash
 from mong import Db
-import os, datetime
+import os, time, datetime
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/home/gary/www/picture-db/imageg'
+app.config['UPLOAD_FOLDER'] = 'static/imageg'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 db = Db()
 
+# 解决mongodb object（ObjectId）不能序列化问题，目前只能用于返回mongo_cursor多个字段
 def replace_images_id(mongo_ob_resp):
     # 多个文件
     images_list = []
@@ -17,43 +18,72 @@ def replace_images_id(mongo_ob_resp):
         image['_id'] = str(id)
         images_list.append(image)
     return jsonify(images_list)
-    
+
+# 获取文件扩展名
 def get_file_ext(filename):
     ext = filename.rsplit('.')[-1]
     return ext
 
+# 首页用来显示所有的图片以及图片所属信息
 @app.route('/')
 def index():
     images = replace_images_id(db.get_all_image())
+    # print(type(images))
     return images
 
+# 显示所有的标签以及相关图片数量
 @app.route('/tags')
 def tags():
     tags = db.get_all_tags()
     return tags
 
+# 显示带有此标签的所有图片
 @app.route('/tags/<string:tag>')
 def find_by_tag(tag):
     images = db.get_images_by_tag(tag)
     images_list = replace_images_id(images)
     return images_list
 
+# 上传图片文件
+#　TODO　上传多个图片，上传时修改标签还是之后统一修改
 @app.route('/upload', methods=("GET", "POST"))
 def upload():
-    msg = ''
+    
     if request.method == 'POST':
+        error = ''
         if 'file' not in request.files:
-            msg = 'No file'
-        
+            error = 'No file'
+            
         file = request.files['file']
         if file.filename == '':
-            msg = 'empty file'
-            
-        if file:
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'test.jpg'))
-        print(msg)
+            error = 'empty file'
+        if error == '':
+            # 文件无问题
+            filename = str(int(time.time())) + '.' + get_file_ext(file.filename)
+            # 保存文件
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # 插入数据库
+            image_info = {
+                'filename':filename,
+                'created_time':datetime.datetime.utcnow(),
+                'uploader':'gary',
+                'rate':5,
+                'group':[],
+                'tags':[]
+
+            }
+            # print(image_info)
+            # 获取图片id
+            id = db.add_new_image(image_info)
+            # print(str(id.inserted_id))
+            # 跳转图片编辑页面
+            return redirect(url_for('edit',id=id.inserted_id))
+        else:
+            flash(error)
     return render_template('upload.html')
 
+# 根据id下载图片
+# TODO 批量下载
 @app.route('/download/<string:id>')
 def download(id):
     image = db.get_image_filename_by_id(id)
@@ -64,17 +94,28 @@ def download(id):
     filename = image['filename']
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
-
-@app.route('/edit/<string:id>')
+#更新图片信息
+@app.route('/edit/<string:id>', methods=("GET", "POST"))
 def edit(id):
+    
     if request.method == 'POST':
-        pass
+    # 通过前端中的表单信息更新图片标签
+    # TODO 刷新重复提交问题，需要检查tag标签的唯一性，用数据库模型方式解决
+    # TODO 前端设置js控件，控制表格提交按钮，防止重复提交
+        id = request.form['id']
+        newTag = request.form['tag']
+        # print(newTag)
+        db.insert_image_tag(id,newTag)
+
+        # 通过重定向改“POST” 为 “GET” 解决刷新重复提交
+        return redirect(url_for('edit', id=id))
     image = db.get_image_filename_by_id(id)
     # print(type(image))
     # image = replace_images_id(image)
     # print(image)
     return render_template('edit.html', image=image)
 
+# 暂时无用,用来更新图片有带
 @app.route('/update/<string:_id>', methods=("GET", "POST"))
 def update(_id):
     return _id
--- mong.py
+++ mong.py
@@ -2,18 +2,20 @@ import pymongo
 from bson.objectid import ObjectId
 
 class Db:
-    def __init__(self, url='mongodb://localhost:27017/', database='test'):
+    # 初始化mongodb数据库
+    def __init__(self, url='mongodb://localhost:27017/', database='test', image_collection='sample'):
           self.connection = pymongo.MongoClient(url)
           self.db = self.connection[database]
-          self.image_collection = self.db['sample']
+          self.image_collection = self.db[image_collection]
           self.user_colletion = self.db['user']
 
+    #查找所有图片,直接返回数据库对象,包含多个对象
     def get_all_image(self):
         images = self.image_collection.find()
 
         return images
 
-
+    #返回所有tag列表和所包含图片数量
     def get_all_tags(self):
         tags = {}
         images = self.image_collection.find()
@@ -26,12 +28,30 @@ class Db:
         
         return tags
 
+    #根据tag查找图片
     def get_images_by_tag(self, tag):
         images = self.image_collection.find({'tags':tag})
 
         return images
 
+    #根据id返回指定图片
     def get_image_filename_by_id(self, id):
         image = self.image_collection.find_one({'_id':ObjectId(id)})
         # print(image)
         return image
+
+    #为图片插入标签
+    def insert_image_tag(self, id, tag):
+        image = self.get_image_filename_by_id(id)
+        
+        #检测标签是否存在
+        if tag not in image['tags']:
+            image['tags'].append(tag)
+            self.image_collection.update_one({'_id':ObjectId(id)}, { "$set": {'tags':image['tags']} })
+        
+        # print(image)
+
+    # 增加新的图片对象, 返回图片id
+    # TODO 增加多个图片
+    def add_new_image(self, image_info):
+        return self.image_collection.insert_one(image_info)
